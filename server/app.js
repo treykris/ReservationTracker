a1a35c015d96b3c8010b09a4d84d60a2058033d8
treykris <trey10027@gmail.com>
2019-07-14 11:02:15 -0400

    Added sms route for twilio messaging and response

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var reservationsRouter = require('./routes/reservations');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const reservationsRouter = require('./routes/reservations');
+const parseMessage = require('./utils/parseMessage');
+const createReservation = require('./db/createReservation');
+const getRestaurantInfo = require('./db/getRestaurantInfo');
+const app = express();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/reservations', reservationsRouter);
+
+/**
+ *
+ * Users will send us their restaurant inquiries in a format separated by " - "
+ *
+ * Format:
+ * `RestaurantRequest - Name - Date - Time`
+ * `RestaurantRequest - Restaurant`
+ *
+ * Examples:
+ * Reservation format:
+ * `Reserveration - Jane Doe - June 4, 2019 - 4pm`;
+ *
+ * Information about restaurant format:
+ * `Information - Peter Luger`
+ *
+ */
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  const twilioData = req.body;
+  const [restaurantRequest, ...message] = parseMessage(req.body.Body);
+  const restaurantRequestLowerCase = restaurantRequest.toLowerCase();
+  let statusMessage;
+
+  if (restaurantRequestLowerCase === 'reservation')
+    statusMessage = createReservation(message, twilioData);
+
+  if (
+    restaurantRequestLowerCase === 'information' ||
+    restaurantRequestLowerCase === 'info'
+  )
+    statusMessage = getRestaurantInfo(message);
+
+  twiml.message(statusMessage);
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
