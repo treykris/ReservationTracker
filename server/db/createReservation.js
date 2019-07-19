9d4d2367088637f85a221da13e5d4d49b7485207
treykris <trey10027@gmail.com>
2019-07-19 13:20:01 -0400

    Updated to use Mongoose Model

--- server/db/createReservation.js
+++ server/db/createReservation.js
@@ -1,61 +1,47 @@
 const moment = require('moment');
-const {db, Reservations} = require('./models/index');
-
-// Reservation - Restaurant - Name -
+const Reservation = require('./models/reservations');
+const mockRestaurants = require('./mockRestaurants');
+const ReservationResponse = require('../constants').ReservationResponse;
 
 const createReservation = async (message, twilioData) => {
   const [name, date, reservationTime] = message;
-  let phone = Number(twilioData.From.slice(1, twilioData.From.length));
-  //console.log(typeof phone);
-  const reservationDateUTC = moment(date, 'MM-DD-YYYY')
+  const reservationDate = moment(date, 'MM-DD-YYYY')
     .utc()
     .valueOf();
-
-  const parsedReservationTime = parseInt(reservationTime, 10);
-  //console.log(parsedReservationTime);
+  const parsedReservationTime = parseInt(reservationTime, 10) + 12;
   const reservationRecord = {
     name,
     time: parsedReservationTime,
-    phone,
-    date: reservationDateUTC
+    phone: twilioData.From,
+    date: reservationDate
   };
   const todaysDate = Date.now();
+  const hoursToMS = 1000 * 60 * 60 * 24;
+  const {closesAt, opensAt} = mockRestaurants[0];
+  const oneHour = 1;
 
-  if (reservationDateUTC < todaysDate) {
-    //console.log('heeeeee');
-    return `Looks like you made a mistake on the date. Can't make a reservation for a past date.`;
+  if (reservationDate < todaysDate) {
+    return ReservationResponse.PAST_DATE;
   }
 
-  const closingTime = 10;
-  const openingTime = 1;
-
-  if (
-    parsedReservationTime < openingTime ||
-    parsedReservationTime > closingTime
-  ) {
-    //console.log('beeeee');
-    return 'Sorry the restaurant is closed at that time.';
-  }
+  if (parsedReservationTime < opensAt || parsedReservationTime >= closesAt)
+    return ReservationResponse.CLOSED;
 
   try {
-    await Reservations.create(reservationRecord);
-    // const findRes = await Reservations.findOne({
-    //   where: {
-    //     date: {
-    //       $eq: reservationDateUTC
-    //     }
-    //   }
-    // });
-    // console.log(findRes);
-    // if (findRes) {
-    //   return `Sorry, a reservation for ${reservationTime} has already been taken.`;
-    // } else {
-    // await Reservations.create(reservationRecord);
-    console.log(`Reservation at ${reservationTime} on ${date} confirmed.`);
-    return `Reservation at ${reservationTime} on ${date} confirmed.`;
-    // }
+    const takenReservation = await Reservation.findOne({
+      date: reservationDate,
+      $and: [
+        {time: {$gte: parsedReservationTime}},
+        {time: {$lt: parsedReservationTime + oneHour}}
+      ]
+    });
+    if (takenReservation) return ReservationResponse.FILLED(reservationTime);
+
+    const newReservation = await Reservation.create(reservationRecord);
+    if (newReservation)
+      return ReservationResponse.CONFIRMED(reservationTime, date);
   } catch (error) {
-    return `Sorry, there was an issue creating your reservation`;
+    return ReservationResponse.ISSUE;
   }
 };
 module.exports = createReservation;
