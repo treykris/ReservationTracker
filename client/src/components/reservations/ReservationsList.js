832fc1c14696fa58b5a0f364ee8b5fb7e4cb4278
treykris <trey10027@gmail.com>
2019-07-19 13:19:37 -0400

    Display reservation information

--- client/src/components/reservations/ReservationsList.js
+++ client/src/components/reservations/ReservationsList.js
@@ -8,16 +8,26 @@ class ReservationsList extends Component {
   componentDidMount() {
     fetch('/reservations')
       .then(res => res.json())
-      .then(reservations => this.setState({reservations}));
+      .then(reservations => this.setState({reservations}))
+      .catch(error => console.log(error));
   }
 
   render() {
+    const {reservations} = this.state;
+    const listOfReservations = reservations.map(reservation => {
+      return (
+        <div key={reservation.id}>
+          <h1>{reservation.name}</h1>
+          <p>{reservation.date}</p>
+          <p>{reservation.time}</p>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
         <h1>Reservations</h1>
-        {this.state.reservations.map(reservation => (
-          <div key={reservation.id}>{reservation.name}</div>
-        ))}
+        {listOfReservations}
       </div>
     );
   }
